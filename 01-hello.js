99514f7a41f7a09a86a27cfa0e3b60bae0378917
yosais <yosais@kfupm.edu.sa>
2023-03-25 17:09:14 +0300

    Initial commit

---
+++ 01-hello.js
@@ -0,0 +1,9 @@
+// Single line comment
+
+/*
+  Multiline comment
+*/
+
+// This is the hello world program in JavaScript
+
+console.log('Hello World');
---
+++ README.md
@@ -0,0 +1,3 @@
+# COE453-T222-Overview-of-JavaScript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jmwmme)
