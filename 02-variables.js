b14f7e3121813889a207f522f7c63dc8ef75e1f5
yosais <yosais@kfupm.edu.sa>
2023-03-25 17:50:36 +0300

    1 file modified

--- 02-variables.js
+++ 02-variables.js
@@ -1,3 +1,17 @@
-/*
-  
-*/
+// There are 4 ways to declare variables in JavaScript
+
+// 1. Using var
+
+var a = 1;
+console.log(a);
+
+// 2. Using let
+
+let b = 2;
+console.log(b);
+
+// 3. Using const
+// Value of a constant cannot be changed after initialization.
+
+const c = 3;
+console.log(c);
