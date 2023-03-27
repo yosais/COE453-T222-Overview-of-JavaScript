e917cb20f97f32b3622028722fadef93a8f1f593
yosais <yosais@kfupm.edu.sa>
2023-03-28 02:49:43 +0300

    1 file modified

--- 04-complex-data-types.js
+++ 04-complex-data-types.js
@@ -1,5 +1,19 @@
 // Array
+// All elements are of the same type
+let zipcodes = ['03255', '02134', '08002'];
+console.log(zipcodes[0]);
+
+let coordinates = [
+  [1, 2],
+  [3, 4],
+];
+console.log(coordinates[0]); // Get first row
+console.log(coordinates[0][0]); // Get first element in first row
 
 // Tuple
+// An array of fixed size that can have different types of elements
+let profile = ['User', 39, true, 15.44];
 
-// Object
+// Object (key-value pairs)
+let car1 = { make: 'Toyota', model: 'Camry', year: 2020 };
+console.log(car1.make);
