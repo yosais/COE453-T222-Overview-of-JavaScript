39311c8ccf9f2c4025c34ee9531e209c5510201e
yosais <yosais@kfupm.edu.sa>
2023-03-28 02:51:54 +0300

    1 file modified

---
+++ 05-if-statements.js
@@ -0,0 +1,16 @@
+let x = 10,
+  y = 20;
+if (x < y) {
+  console.log('x is less than y');
+} else if (x == y) {
+  console.log('x is equal to y');
+} else {
+  console.log('x is greater than y');
+}
+
+console.log(3 != 5);
+
+// Logical operators
+console.log(7 > 5 && 2 > 1);
+console.log(7 > 5 || 0 > 1);
+console.log(!(7 > 5));
