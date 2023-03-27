ee9ad3db621af4bb215b0b931b8baaa33debcc1d
yosais <yosais@kfupm.edu.sa>
2023-03-28 02:41:35 +0300

    1 file modified

--- 03-primitive-data-types.js
+++ 03-primitive-data-types.js
@@ -1,10 +1,23 @@
 // Number
+let int = 3;
+let decimal = 6.1;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
 
 // Big integers
+let a = 999999999999999999999999999999999999999;
+console.log(`a = ${a}`);
 
 // Boolean
+let isEnabled = true;
+let isAlive = false;
 
 // String
+let fname = 'Ahmad';
+let lname = 'Ali';
+let message = 'Your name is ' + fname + ' ' + lname;
+console.log(message);
 
 // Undefined
 
