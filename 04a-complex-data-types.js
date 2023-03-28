ffb803c8a56105a64a15f32a9f0eba56da10d0d9
yosais <yosais@kfupm.edu.sa>
2023-03-28 17:14:25 +0300

    2 files added, 1 deleted

--- 04a-complex-data-types.js
+++ 04a-complex-data-types.js
@@ -15,10 +15,23 @@ console.log(coordinates[0][0]); // Get first element in first row
 let profile = ['User', 39, true, 15.44];
 
 // Set
+const mySet1 = new Set();
+mySet1.add(1); // Set(1) { 1 }
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add(5); // Set(2) { 1, 5 }
 
-// Map & Filter
+mySet1.has(1); // true
+mySet1.has(3); // false
+
+mySet1.size; // 3
+
+mySet1.delete(5); // removes 5 from the set
+
 
 // Date
+const today = new Date();
+console.log(today.toString());
+
 
 // Object (key-value pairs)
 let car1 = { make: 'Toyota', model: 'Camry', year: 2020 };
