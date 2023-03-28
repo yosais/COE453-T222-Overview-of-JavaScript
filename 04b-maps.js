55b092976324e17ee9c61623412932a8069964cc
yosais <yosais@kfupm.edu.sa>
2023-03-28 17:38:00 +0300

    1 file modified

--- 04b-maps.js
+++ 04b-maps.js
@@ -18,6 +18,7 @@ let officers = [
   { id: 88, name: 'Commander Jerjerrod' },
 ];
 
+// Map
 // Extract all id values
 let officersIds = [];
 officers.forEach(function (officer) {
@@ -32,3 +33,106 @@ console.log(officersIds_2);
 
 const officersIds_3 = officers.map((officer) => officer.id);
 console.log(officersIds_3);
+
+// Reduce
+// Get total number of years of experience
+var pilots = [
+  {
+    id: 10,
+    name: 'Poe Dameron',
+    years: 2,
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 3,
+  },
+  {
+    id: 41,
+    name: 'Tallissan Lintra',
+    years: 1,
+  },
+  {
+    id: 99,
+    name: 'Ello Asty',
+    years: 2,
+  },
+];
+
+const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
+console.log(totalYears);
+
+// Filter
+var pilots = [
+  {
+    id: 2,
+    name: 'Wedge Antilles',
+    faction: 'Rebels',
+  },
+  {
+    id: 8,
+    name: 'Ciena Ree',
+    faction: 'Empire',
+  },
+  {
+    id: 40,
+    name: 'Iden Versio',
+    faction: 'Empire',
+  },
+  {
+    id: 66,
+    name: 'Thane Kyrell',
+    faction: 'Rebels',
+  },
+];
+
+const rebels = pilots.filter((pilot) => pilot.faction === 'Rebels');
+const empire = pilots.filter((pilot) => pilot.faction === 'Empire');
+console.log(rebels);
+console.log(empire);
+
+// Combining map, reduce, and filter
+var personnel = [
+  {
+    id: 5,
+    name: 'Luke Skywalker',
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true,
+  },
+  {
+    id: 82,
+    name: 'Sabine Wren',
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false,
+  },
+  {
+    id: 22,
+    name: 'Zeb Orellios',
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false,
+  },
+  {
+    id: 15,
+    name: 'Ezra Bridger',
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true,
+  },
+  {
+    id: 11,
+    name: 'Caleb Dume',
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true,
+  },
+];
+
+let totalPilotScoreForForceUser = personnel
+  .filter((person) => person.isForceUser)
+  .map((person) => person.pilotingScore)
+  .reduce((acc, score) => acc + score, 0);
+
+console.log(totalPilotScoreForForceUser);
